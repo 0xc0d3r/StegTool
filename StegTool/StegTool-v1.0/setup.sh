98dd595ef1d74abae8b1bab7c76a0e453a748a16
0xc0d3r <anesh.parvatha@gmail.com>
2014-08-27 16:59:23 +0530

    Documentation is uploaded

--- StegTool/StegTool-v1.0/setup.sh
+++ StegTool/StegTool-v1.0/setup.sh
@@ -1,10 +1,10 @@
 echo "[+] Setuping stegtool...";
 echo "[+] Compiling all source files...";
-javac -d . src/util/*.java
-javac -d . src/security/*.java
-javac -d . src/algo/*.java
-javac -d . src/cmd/*.java
-javac -d . src/gui/*.java
+javac -d . src/stegtool/util/*.java
+javac -d . src/stegtool/security/*.java
+javac -d . src/stegtool/algo/*.java
+javac -d . src/stegtool/cmd/*.java
+javac -d . src/stegtool/gui/*.java
 echo "[+] Generating manifest file and adding contents...";
 echo "Manifest-Version: 1.0\nSpecification-Title: \"StegTool\"\nCreated-By: 2.0 (0xC0d3r,J0rd3n,Anil)\nMain-Class: stegtool.gui.MainFrame" > MyManifest.txt
 echo "[+] Creating archive(.jar) file...";
--- StegTool/StegTool-v1.0/src/util/LSBStatus.java
+++
@@ -1,6 +0,0 @@
-package stegtool.util;
-
-public class LSBStatus {
-    public static boolean embedSuccess = false;
-    public static boolean extractSuccess = false;
-}
